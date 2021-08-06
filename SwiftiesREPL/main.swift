685b5b94fd3991aec08444ee995e085a4af6c97a
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-06 23:50:14 +0200

    Added int reader.

--- SwiftiesREPL/main.swift
+++ SwiftiesREPL/main.swift
@@ -13,7 +13,7 @@ try env.coreLib!.bind(pos: initPos)
 try MathLib(env: env, pos: initPos).bind(pos: initPos)
 
 let parser = Parser(env: env, source: "repl",
-                    spaceReader, callReader, idReader)
+                    spaceReader, callReader, intReader, idReader)
 
 var prompt = 1
 var input = ""
