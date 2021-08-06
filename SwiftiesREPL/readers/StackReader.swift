89d991643dabc4e88285f8e44169a548cb2462f4
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-07 00:54:55 +0200

    Added stack reader.

---
+++ SwiftiesREPL/readers/StackReader.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Swifties
+
+public let stackReader = StackReader()
+
+public class StackReader: Reader {
+    public func readForm(_ p: Parser) throws -> Form? {
+        let fpos = p.pos
+        var c = p.getc()
+        
+        if c != "[" {
+            if c != nil { p.ungetc(c!) }
+            return nil
+        }
+        
+        p.nextColumn()
+        var items: [Form] = []
+
+        while true {
+            try spaceReader.readForm(p)
+            c = p.getc()
+            if c == nil || c == "]" { break }
+            p.ungetc(c!)
+            let f = try parser.readForm(p)
+            if f == nil { break }
+            items.append(f!)
+        }
+
+        if c != "]" { throw ReadError(p.pos, "Open stack form: \((c == nil) ? "nil" : String(c!))") }
+        p.nextColumn()
+        return StackForm(env: p.env, pos: fpos, items: items)
+    }
+}
